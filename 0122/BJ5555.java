a2a73559a714ffdd739c50104e5ab81a45fffcd0
jwbae0820 <45513215+jwbae0820@users.noreply.github.com>
2020-01-22 14:56:54 +0900

    0122 반지

--- 0122/BJ5555.java
+++ 0122/BJ5555.java
@@ -4,7 +4,7 @@ public class BJ5555 {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int count = 0;
+	int count = 0;
         String str = sc.next();
         int N = sc.nextInt();
         
