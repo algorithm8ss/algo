e9cc9edbd5bf9caec57075ccfe37b0c697660ba8
Karina008 <30279913+Karina008@users.noreply.github.com>
2020-01-23 14:08:39 +0900

    0122_설탕

---
+++ 0122/5555_설탕_배달_pjw.java
@@ -0,0 +1,28 @@
+package study;
+import java.util.Scanner;
+
+public class 설탕_배달 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int N= sc.nextInt();
+		int count=0;
+		while(N>0) {
+			if(N%5==0) {
+				count+=N/5;
+				N=0;
+			}
+			else {
+				N=N-3;
+				count++;
+			}
+		
+			
+		}
+		
+		if(N<0) 
+			System.out.println("-1");		
+		else
+			System.out.println(count);
+	}
+}
