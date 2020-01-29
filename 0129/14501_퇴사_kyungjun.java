482a76618df7bc0667500973402a6356e86724ea
ykj0202 <60208657+ykj0202@users.noreply.github.com>
2020-01-29 21:44:48 +0900

    Update 14501_퇴사_kyungjun.java

--- 0129/14501_퇴사_kyungjun.java
+++ 0129/14501_퇴사_kyungjun.java
@@ -61,11 +61,4 @@ public class ts {
 	        comb(arr, visited, depth + 1, n, r);
 	    }
 	}
-    static void print(int[] arr, boolean[] visited, int n) {
-        for(int i=0; i<n; i++) {
-            if(visited[i] == true)
-                System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
 }
